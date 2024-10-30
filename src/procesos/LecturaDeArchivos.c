f3fda8210039e65774dc761a93c4165b59853d18
Luis-For <luisfor90@gmail.com>
2024-10-29 20:12:38 -0500

    Actualizacion de las diferentes lecturas de archivos

--- src/procesos/LecturaDeArchivos.c
+++ src/procesos/LecturaDeArchivos.c
@@ -1,4 +1,66 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+// Función para leer una matriz desde un archivo
+void leerMatriz(char *nombreArchivo, int filas, int columnas, int matriz[filas][columnas]) {
+    FILE *archivo = fopen(nombreArchivo, "r");
+    if (archivo == NULL) {
+        printf("Error al abrir el archivo %s\n", nombreArchivo);
+        return;
+    }
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            fscanf(archivo, "%d", &matriz[i][j]);
+        }
+    }
+    fclose(archivo);
+}
+
+// Función para leer un vector desde un archivo
+void leerVector(char *nombreArchivo, int longitud, int vector[longitud]) {
+    FILE *archivo = fopen(nombreArchivo, "r");
+    if (archivo == NULL) {
+        printf("Error al abrir el archivo %s\n", nombreArchivo);
+        return;
+    }
+    for (int i = 0; i < longitud; i++) {
+        fscanf(archivo, "%d", &vector[i]);
+    }
+    fclose(archivo);
+}
+
+// Función para leer una cadena desde un archivo
+void leerCadena(char *nombreArchivo, char *cadena, int longitud) {
+    FILE *archivo = fopen(nombreArchivo, "r");
+    if (archivo == NULL) {
+        printf("Error al abrir el archivo %s\n", nombreArchivo);
+        return;
+    }
+    fgets(cadena, longitud, archivo);
+    fclose(archivo);
+}
+// Función para mostrar una matriz
+void mostrarMatriz(int filas, int columnas, int matriz[filas][columnas]) {
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// Función para mostrar un vector
+void mostrarVector(int longitud, int vector[longitud]) {
+    for (int i = 0; i < longitud; i++) {
+        printf("%d ", vector[i]);
+    }
+    printf("\n");
+}
+
+// Función para mostrar una cadena
+void mostrarCadena(char *cadena) {
+    printf("%s\n", cadena);
+}
 
 int main() {
     FILE *file = fopen("archivo.txt", "r");
