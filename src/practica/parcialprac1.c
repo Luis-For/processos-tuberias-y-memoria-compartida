7073b415682581fa2431c9bbd8a61dba175e6b9f
Luis-For <luisfor90@gmail.com>
2024-11-15 15:02:46 -0500

    Actualizacion

--- src/practica/parcialprac1.c
+++ src/practica/parcialprac1.c
@@ -7,3 +7,27 @@
 #include <unistd.h>
 #include <string.h>
 
+void creationsOfChildrens(int childrens, pid_t root){
+    for (int i = 0; i < childrens; i++)
+    {
+        if(!fork()) break;
+    }
+    if(root==getpid()){
+        tree(root);
+    }
+}
+
+void tree(pid_t pid){
+    char command[50];
+    sprintf(command, "pstree -pl %d", pid);
+    system(command);
+}
+
+int main(int argc, char const *argv[])
+{
+    const pid_t ROOT=getpid();
+    creationsOfChildrens(3,ROOT);
+    //tree(ROOT);
+    
+    return 0;
+}
